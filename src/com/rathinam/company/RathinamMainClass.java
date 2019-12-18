fad1f23a75b49547843a0b8991ac8d0271869771
thoma <thoma@Thomas.Home>
2019-12-18 22:25:32 +0000

    Team Project

---
+++ src/com/rathinam/company/RathinamMainClass.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package com.rathinam.company;
+
+/**
+ * @author thoma
+ *
+ */
+public class RathinamMainClass {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Jesus is my god");
+
+	}
+
+}
